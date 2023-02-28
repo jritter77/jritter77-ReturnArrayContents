386e96e7af583ad5be6d0edf3d65e0ad9101a68c
Jake Ritter <jr550@humboldt.edu>
2023-02-28 18:40:38 +0000

    Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String result = "";
+
+        for (int num : nums) {
+            result += num + " ";
+        }
+
+        return result;
     }
 }
